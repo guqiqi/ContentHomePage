f5958b1199cb3b1d507bec5e64856672e1933c8e
张凌哲 <445073309@qq.com>
2019-05-15 08:36:00 +0800

    edit config again

--- restart.sh
+++ restart.sh
@@ -1,3 +1,7 @@
 sudo cp -r frontend /var/www/html/herencup
 sudo cp -r admin-frontend /var/www/html/admin
+cd herencup
+mvn clean package
+sudo nohup java -jar target/herencup-1.0-SNAPSHOT.jar &
+cd ..
 /etc/init.d/nginx start
